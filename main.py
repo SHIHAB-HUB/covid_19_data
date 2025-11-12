aeb31d0c195015ae00b727ce77ad29135e609554
SHIHAB-HUB <si.shihab2005@gmail.com>
2025-11-12 15:37:28 +0600

    update

--- main.py
+++ main.py
@@ -14,13 +14,9 @@ def calc_total(df, case_name):
 def sort_df(df, case_name):
     return df.sort_values(case_name, ascending=False)
 
-<<<<<<< HEAD
 # ============================
 # BAR CHART DRAWING
 # ============================
-=======
-
->>>>>>> 9ea63f46a36a3e62d226c2d3e01b88a5d9e89989
 def draw_bar_chart(x_axis, y_axis, colours, title, x_label, y_label):
     plt.barh(x_axis, y_axis, color=colours)
     plt.title(title)
@@ -29,7 +25,6 @@ def draw_bar_chart(x_axis, y_axis, colours, title, x_label, y_label):
 
     # Add value labels beside each bar (with commas for readability)
     for index, value in enumerate(y_axis):
-<<<<<<< HEAD
         plt.text(value, index, f"{float(value):,.2f}", va='center')
 
     plt.tight_layout()
@@ -66,16 +61,10 @@ def subplt_country_vs_cases(df):
             for index, value in enumerate((sorted_df[i][j])[cases[i][j]]):
                 axis[i,j].text(value, index, f"{float(value):,.2f}", va='center')
          
-    figure.suptitle("“Global COVID-19 Overview – Top 5 Countries Comparison", size=20, color="red")
+    figure.suptitle("“Global COVID-19 Overview – Top 10 Countries Comparison", size=20, color="red")
     plt.tight_layout()
     plt.show()
     
-=======
-        plt.text(value, index, f"{int(value):,}", va='center')
-
-    plt.tight_layout()
-    plt.show()
->>>>>>> 9ea63f46a36a3e62d226c2d3e01b88a5d9e89989
     
     
 # ========================
@@ -103,13 +92,9 @@ def bar_chart_total_cases(df):
     colours = np.array(["#3498db", "#0963ff", "#006aff", "#0011ff"])
 
     # Create bar chart
-<<<<<<< HEAD
     draw_bar_chart(x_axis=cases_df["Category"], y_axis=cases_df["Values"],
                    colours=colours, title="Global COVID-19 Total Cases Overview",
                    x_label="Number of Cases", y_label="Category")
-=======
-    draw_bar_chart(x_axis=cases_df["Category"], y_axis=cases_df["Values"], colours=colours, title="Global COVID-19 Total Cases Overview", x_label="Number of Cases", y_label="Category")
->>>>>>> 9ea63f46a36a3e62d226c2d3e01b88a5d9e89989
     
 # =================================
 # BAR CHART WITH COUNTRY AND CASE
@@ -120,25 +105,20 @@ def bar_chart_with_sorted(df, case_name):
     colours = np.array(["#f73e3e", "#f7633e", "#faa72a", "#facc00", "#53fa00",
                         "#0bea77", "#00fae1", "#00affa", "#002afa", "#5700fa"])
 
-<<<<<<< HEAD
     draw_bar_chart(x_axis=sorted_df["Country/Region"], y_axis=sorted_df[case_name],
                    colours=colours, title=f"Global COVID-19 Country/Region and {case_name} Overview",
                    x_label=f"Values of {case_name}", y_label=f"Country/Region")
-=======
-    draw_bar_chart(x_axis=sorted_df["Country/Region"], y_axis=sorted_df[case_name], colours=colours, title=f"Global COVID-19 Country/Region and {case_name} Overview", x_label=f"Values of {case_name}", y_label=f"Country/Region")
->>>>>>> 9ea63f46a36a3e62d226c2d3e01b88a5d9e89989
     
     
 def main():
     file_path = "data_set/Country_wise_latest.csv"
     df = pd.read_csv(file_path)
-<<<<<<< HEAD
     
-    # bar_chart_total_cases(df)
-    # bar_chart_with_sorted(df, "Confirmed")
-    # bar_chart_with_sorted(df, "Deaths / 100 Cases")
-    # bar_chart_with_sorted(df, "Recovered / 100 Cases")
-    # bar_chart_with_sorted(df, "Deaths / 100 Recovered")
+    bar_chart_total_cases(df)
+    bar_chart_with_sorted(df, "Confirmed")
+    bar_chart_with_sorted(df, "Deaths / 100 Cases")
+    bar_chart_with_sorted(df, "Recovered / 100 Cases")
+    bar_chart_with_sorted(df, "Deaths / 100 Recovered")
     
     subplt_country_vs_cases(df)
     
@@ -146,11 +126,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-=======
-    bar_chart_total_cases(df)
-    
-    bar_chart_with_sorted(df, "Confirmed")
-    
-if __name__ == "__main__":
-    main()
->>>>>>> 9ea63f46a36a3e62d226c2d3e01b88a5d9e89989
